cafb137a9133ceaa2ab4a3f07af6dd8ae2edd70d
Luillo <labaroasordo@gmail.com>
2023-07-25 22:06:16 -0600

    Adding logos

---
+++ frontend/app/lib/core/utils/constants/assets.dart
@@ -0,0 +1,6 @@
+const String logo1 = 'assets/logos/Logo_StageBuzz-1.png';
+const String logo2 = 'assets/logos/Logo_StageBuzz-2.png';
+const String logo3 = 'assets/logos/Logo_StageBuzz-3.png';
+const String logo4 = 'assets/logos/Logo_StageBuzz-4.png';
+const String logo5 = 'assets/logos/Logo_StageBuzz-5.png';
+const String logo6 = 'assets/logos/Logo_StageBuzz-6.png';
